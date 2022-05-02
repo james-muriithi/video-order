ca6b4167708b516bf9d4582c58bcb4ad6b515dec
james-muriithi <muriithijames556@gmail.com>
2022-05-03 00:46:26 +0300

    added navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-0 fw-bold">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="#">
+        <Logo />
+        <span class="ms-2 fw-bold fs-6">Video Order</span>
+      </router-link>
+      <button
+        class="navbar-toggler border-0"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+          <li
+            class="nav-item"
+            v-for="{ title, link } in menuItems"
+            :key="title"
+          >
+            <router-link class="nav-link" aria-current="page" :to="link" active-class="active">
+              {{ title }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Logo from "./Logo.vue";
+/* eslint-disable */
+export default {
+  components: { Logo },
+  name: "Navbar",
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "Home",
+          link: "/",
+        },
+        {
+          title: "Popular",
+          link: "#",
+        },
+        {
+          title: "About",
+          link: "#",
+        },
+        {
+          title: "Account",
+          link: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router';
 
-Vue.config.productionTip = false
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import "@/assets/scss/styles.scss";
+
+import Navbar from "@/components/Navbar.vue"
+
+Vue.config.productionTip = false;
+// eslint-disable-next-line
+Vue.component('Navbar', Navbar)
 
 new Vue({
   router,
