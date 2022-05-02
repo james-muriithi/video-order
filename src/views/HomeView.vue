a443464fbaa0488fa0a58c857f56cd0d6f935c60
james-muriithi <muriithijames556@gmail.com>
2022-05-03 02:22:08 +0300

    added home search image

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container-fluid h-100">
-    <div class="row h-100 align-items-center justify-content-center">
+    <div class="row h-100 align-items-center justify-content-center pt-5">
       <div class="col-sm-10 col-md-8 col-xl-6">
         <SearchBar />
+        <div class="text-center">
+        <SearchImage class="mt-4" />
+      </div>
       </div>
     </div>
   </div>
@@ -10,9 +13,10 @@
 
 <script>
 import SearchBar from '@/components/SearchBar.vue'
+import SearchImage from '@/components/SearchImage.vue'
 
 export default {
-  components: { SearchBar },
+  components: { SearchBar, SearchImage },
   name: 'HomeView',
 }
 </script>
