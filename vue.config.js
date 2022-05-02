e0b638fc7b54e8335105c722e2cf76933f9166e7
james-muriithi <muriithijames556@gmail.com>
2022-05-02 22:44:31 +0300

    initial setup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Video Order";
+      return args;
+    });
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "@/assets/scss/partials/_variables.scss"; @import "@/assets/scss/partials/_colors.scss"; @import "@/assets/scss/partials/_mixins.scss";`,
+        sourceMap: true,
+      },
+    },
+  },
 })
